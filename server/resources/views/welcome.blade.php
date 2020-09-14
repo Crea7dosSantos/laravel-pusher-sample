4c09d76d61d956fa5d3eb0ea23f1ec42fb0ef9b5
Crea7dosSantos <cr26yusaku0720@icloud.com>
2020-09-14 16:34:57 +0900

    close #4

--- server/resources/views/welcome.blade.php
+++ server/resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <title>laravel-pusher-sample</title>
+    <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
 </head>
 
 <body>
@@ -23,6 +24,16 @@
         </form>
     </div>
     <script>
+        Pusher.logToConsole = true;
+
+        var pusher = new Pusher('1b917830469415ec57ef', {
+            cluster: 'ap3'
+        });
+
+        var channel = pusher.subscribe('my-channel')
+        channel.bind('my-event', function(data) {
+            alert(JSON.stringify(data))
+        });
     </script>
 </body>
 
